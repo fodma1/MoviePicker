c596765b6e4d39ab56253919128efcafbc4adac6
Matyas Fodor <matyas.fodor@gmail.com>
2017-06-03 08:56:44 +0100

    Fix async storage access, add test db

--- src/app.js
+++ src/app.js
@@ -1,15 +1,64 @@
 import React, { Component } from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, Text, AsyncStorage } from 'react-native';
 
 import {
   StackNavigator,
 } from 'react-navigation';
 
-import Tinder from './Tinder.js'
-
+import Login from './Login';
+import Tinder from './Tinder';
+import KeyNameSpace from './consts';
 
 const MoviePicker = StackNavigator({
   Tinder: { screen: Tinder },
 });
 
-export default MoviePicker;
+class MainDispatch extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        loaded: false,
+        user: null,
+      };
+  }
+
+  componentDidMount() {
+    this._authenticate();
+  }
+
+  _onLogin(token) {
+    console.log('login: ', token);
+    this.setState({user:token});
+  }
+
+  async _authenticate() {
+    console.log('auithenticate');
+    const valami = await AsyncStorage.setItem(`${KeyNameSpace}:token`, 'non null');
+    console.log('valami');
+    try {
+      const token = await AsyncStorage.getItem(`${KeyNameSpace}:token`);
+      if (token !== null) {
+        this.setState({ user: token });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    this.setState({ loaded: true });    
+  }
+
+  render() {
+    if (this.state.loaded) {
+      if (this.state.user) {
+        return (<MoviePicker/>);
+      } else {
+        return (
+          <Login onLogin={(d) => this._onLogin(d)}/>
+        );
+      }
+    } else {
+      return (<Text>Not loaded</Text>);
+    }
+  }
+}
+
+export default MainDispatch;
